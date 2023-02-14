7f4b6ea44483efac64062016ea6b34ccbe013829
Nariman Huseynov <hnariman@gmail.com>
2023-02-15 01:54:38 +0400

    added one more passing test

--- test/init.test.ts
+++ test/init.test.ts
@@ -4,6 +4,9 @@ describe("shall run some test", () => {
   it("shall not fail", () => {
     expect(true).toBeTruthy;
   });
+  it("shall increase count on passing tests badge", () => {
+    expect(false).toBeFalsy;
+  });
   it("shall greet", () => {
     expect(greet("name")).toEqual("we are the name");
   });
